660808c24cd95959ec2edc80823cc94928fef847
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-09-20 14:16:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
-function setup() {
+function setup() {createCanvas (600,600)
+    background (0,0,0)
 }
 
-function draw() {
+function draw() {fill(255,255,255)
+    triangle (150,0,300,300,450,0)
+    triangle (150,600,300,300,450,600)
+    triangle (150,0,225 ,150,50,150)
+    triangle (550,450,375,450,450,600)
+triangle(450,450,450,300,600,300)
+strokeWeight(0)
+triangle (450,450,550,450,600,300)
+triangle (150,150,150,300,0,300)
+strokeWeight(0)
+triangle (150,150,50,150,0,300)
 }
